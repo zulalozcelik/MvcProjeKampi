a3e3188d776056efaac04f5857e9b24595d1937f
Zülal Özçelik <zllzclk17@gmail.com>
2024-11-04 17:25:34 +0300

    04.11.2024

---
+++ DataAccesLayer/Abstarct/ICategoryDal.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayerr.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Abstarct
+{
+    public interface ICategoryDal:IRepository<Category>
+    {
+       
+    }
+}
---
+++ DataAccesLayer/Abstarct/IWriterDal.cs
@@ -0,0 +1,13 @@
+﻿using EntityLayerr.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Abstarct
+{
+    public interface IWriterDal :IRepository<Writer>
+    {
+    }
+}
